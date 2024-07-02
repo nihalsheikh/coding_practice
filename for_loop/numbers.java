b6ea165467e015040067e70fc6e7bb6c7fac3622
nihalsheikh <nihalsheikh585@gmail.com>
2024-07-02 15:20:05 +0530

    Print 'n' Natural Numbers

--- for_loop/numbers.java
+++ for_loop/numbers.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class numbers {
     public static void main(String args[]) {
-        //print number
+        // take user input
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
-        for(int i = 0; i <= n; i++) {
+        //loop to print natural number
+        for(int i = 1; i <= n; i++) {
             //display result
             System.out.print(i + "\n");
         }
