ab7bdb87baafec26bb883f2f609026a3be8ec8ad
nihalsheikh <nihalsheikh585@gmail.com>
2024-07-07 19:52:08 +0530

    Pattern 2: Asterisk Rectangle Solved

--- techgig/pattern_1/asterisk_rectangle.java
+++ techgig/pattern_1/asterisk_rectangle.java
@@ -14,7 +14,7 @@ public class asterisk_rectangle {
         // pattern logic
         for (int i = 0; i < n; i++) {   // column loop
             for (int j = 0; j < n-1; j++) {   // row loop
-                System.out.print("* ");
+                System.out.print("*" + " ");
             }
             System.out.println("");
         }
