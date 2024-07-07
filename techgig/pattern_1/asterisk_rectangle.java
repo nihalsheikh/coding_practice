4ba248b260305a36b8b4941828726f195eff85e7
nihalsheikh <nihalsheikh585@gmail.com>
2024-07-07 19:47:49 +0530

    Pattern 2: Asterisk Rectangle Solved

---
+++ techgig/pattern_1/asterisk_rectangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class asterisk_rectangle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        // user input
+        do {
+            n = sc.nextInt();
+        }
+        while (n <= 1 && n <= 1000);
+
+        // pattern logic
+        for (int i = 0; i < n; i++) {   // column loop
+            for (int j = 0; j < n-1; j++) {   // row loop
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
