a66fd02f258cfa7d8f2f0102feb1d01eb257722b
nihalsheikh <nihalsheikh585@gmail.com>
2024-07-08 09:41:46 +0530

    Pattern 3: Incremented Rectangle Solved

--- techgig/pattern_3/rectangle2.java
+++ techgig/pattern_3/rectangle2.java
@@ -11,7 +11,7 @@ public class rectangle2 {
         }
         while (n <= 1 && n <= 1000);
 
-        // pattern logic
+        // Incremented Rectangle pattern logic
         for (int i = 0; i < n; i++) {   // column loop
             for (int j = 0; j < n; j++) {   // row loop
                 System.out.print(j + 1 + " ");  // when you have to increase value in the same line, then use the variable of 2nd loop
